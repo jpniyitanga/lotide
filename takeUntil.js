ed17cf0b6d5b8f9a49d57f3a7c5383206bd51ad7
JP Niyitanga <jpniyitanga@gmail.com>
2023-02-04 12:42:28 -0500

    Initial commit

---
+++ takeUntil.js
@@ -0,0 +1,32 @@
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let item of array) {
+    if (!callback(item)) {
+      result.push(item);
+    }   else {
+      return result;
+    }
+    
+  }
+
+};
+
+// let callbackx = function(item) {
+//   if (item < 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
