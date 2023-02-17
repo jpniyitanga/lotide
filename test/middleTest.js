b7b0ca271ebd2517f4a2d11ab79a188c5a5fb3ba
JP Niyitanga <jpniyitanga@gmail.com>
2023-02-16 23:54:08 -0500

    Replaced assert functions with Mocha and Chai.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
-const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const eqArrays = require('../eqArrays');
+// const middle = require('../middle');
 
-console.log(middle([0,1, 3, 4]));
+// console.log(middle([0,1, 3, 4]));
+
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns [1, 3] for [0, 1, 3, 4]", () => {
+    assert.deepEqual(middle([0, 1, 3, 4]), [1, 3]);
+  });
+
+  
+});
