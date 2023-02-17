069c88908f9eb6a700cadbe8c0358620929b7392
JP Niyitanga <jpniyitanga@gmail.com>
2023-02-16 22:45:59 -0500

    removed test code at the bottom of the function and removed require at the top of the function

--- tail.js
+++ tail.js
@@ -1,5 +1,4 @@
 
-const assertEqual = require('./assertEqual');
 
 let tail = function(array) {
   return array.slice(1);
@@ -8,18 +7,5 @@ let tail = function(array) {
 
 module.exports = tail;
 
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.leng, ["Lighthouse", "Labs"]); // => will always fail!
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
