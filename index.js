b8127019ccea56e73eee9680aed06aef3ce2ebd7
JP Niyitanga <jpniyitanga@gmail.com>
2023-02-17 22:21:19 -0500

    Exported all functions into index.js file

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 
 module.exports = {
@@ -16,5 +25,14 @@ module.exports = {
   assertEqual: assertEqual,
   assertObjectsEqual: assertObjectsEqual,
   countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
 
 };
