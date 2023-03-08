dd8c163aa8b2895547dcba3484f5fc8871e0f894
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-03-08 00:31:19 -0500

    Include the logic inside a function.

--- reverse.js
+++ reverse.js
@@ -1,13 +1,19 @@
-let input = process.argv.slice(2);
-
-for (let i = 0; i < input.length; i++) {
-  let output = ""
-  for (let j = input[i].length; j > -1; j--) {
-    //console.log(input[i][j]);
-    output += input[i].charAt(j);
+const reverse = function() {
+  let input = process.argv.slice(2);
+  for (let i = 0; i < input.length; i++) {
+    let output = "";
+    for (let j = input[i].length; j > -1; j--) {
+      output += input[i].charAt(j);
+    }
+    console.log(output);
   }
-  console.log(output);
-}
+};
+module.exports = reverse;
+
+
+
+
+
 
 
 
