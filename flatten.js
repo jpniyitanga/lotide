0fe8e7df247e2eb5b8a41564e8dbb9a7ee9be340
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-03-08 00:18:24 -0500

    implement the logic without using helper function.

--- flatten.js
+++ flatten.js
@@ -1,10 +1,21 @@
 
-  const eqArrays = require('./eqArrays');
-  const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-  let flatten = function(arrayOfArrays) {
-    return arrayOfArrays.flat();
-  };
+const flatten = function(arrayOfArrays) {
+  // return arrayOfArrays.flat();
+  const newArray = [];
+  for (let i = 0; i < arrayOfArrays.length; i++) {
+    if (Array.isArray(arrayOfArrays[i])) {
+      for (let j = 0; j < arrayOfArrays[i].length; j++) {
+        newArray.push(arrayOfArrays[i][j]);
+      }
+    } else {
+      newArray.push(arrayOfArrays[i]);
+    }
+  }
+  return newArray;
+};
 
-  module.exports = flatten;
+module.exports = flatten;
 
