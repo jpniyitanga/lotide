7ec269146f0f3aec4900464a39d7db7f3f7f4b13
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-03-08 00:22:57 -0500

    Define assertObjectsEqual function.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,40 +1,15 @@
+const eqObjects = require('./eqObjects');
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1); // Initialize a variable for keys of first object
-  const keys2 = Object.keys(object2); // Initialize a variable for keys of second object
-
-  // Compares length of keys1 and keys2
-  if (keys1.length !== keys2.length) {
-    return false;
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
   } else {
-    // cheking whether values are arrays and comparing them
-    for (let key of keys1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      
-      } else if
-      // Comparing values that are not arrays
-      (object1[key] !== object2[key]) {
-        return false;
-      }
-      
-    }
-
+    console.log(
+      `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
   }
-
-  return true;
 };
-
-module.exports = eqObjects;
-
-
-
-
-
-
-
-
+module.exports = assertObjectsEqual;
