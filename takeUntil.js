6c73ad9b519efb1f6c4e9a31d46131c6e0f5f89e
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-03-08 00:21:43 -0500

    Add code to return entire array when none of the elements have a successful response from callback.

--- takeUntil.js
+++ takeUntil.js
@@ -1,26 +1,12 @@
-
-
 const takeUntil = function(array, callback) {
   let result = [];
   for (let item of array) {
     if (!callback(item)) {
       result.push(item);
     }   else {
-      return result;
+      break;
     }
-    
   }
-
+  return result;
 };
-
-// let callbackx = function(item) {
-//   if (item < 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
 module.exports = takeUntil;
-
-
